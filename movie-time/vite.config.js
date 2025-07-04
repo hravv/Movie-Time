1270f78f68f898c444908e676ec28f227985a88d
hravv <harveyoliverburman@outlook.com>
2025-07-04 09:25:26 +0000

    completed deploy

--- movie-time/vite.config.js
+++ movie-time/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/movie-time/',
+  base: '/Movie-Time/',
   plugins: [
     react(),
     tailwindcss(),
